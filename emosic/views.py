0bd7419b22e045dd8aaee3f64a1e294087c80ebb
ajay kundu <ajaykunduktl2@gmail.com>
2017-11-21 19:24:45 +0530

    Most of the Setting have been done

--- emosic/views.py
+++ emosic/views.py
@@ -24,6 +24,7 @@ class HomePage(TemplateView):
 
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.conf import settings
 
 @csrf_exempt
@@ -33,7 +34,7 @@ def save_image(request):
         f.write(request.body)
         f.close()
         print('something found')
-        return render(re)
+        return HttpResponse('http://127.0.0.1:8000/media/webcamimages/someimage.jpeg')
     else:
         print('not get any data.')
         return HttpResponse('no data')
@@ -41,8 +42,13 @@ def save_image(request):
 
 import http.client, urllib.request, urllib.parse, urllib.error, base64, sys
 import json
+from musicapp import views
+from musicapp.models import Song
 
 def EmotionTemp(request):
+
+    songList = Song.objects.all()
+
     print('hello')
     headers = {
         # Request headers. Replace the placeholder key below with your subscription key.
@@ -51,8 +57,12 @@ def EmotionTemp(request):
     }
     params = urllib.parse.urlencode({
     })
+    pathtoImage = settings.MEDIA_ROOT + '/webcamimages/someimage.jpeg'
+    pathtoImage = 'https://engineering.unl.edu/images/staff/Kayla_Person-small.jpg'
+    print(pathtoImage)
     # Replace the example URL below with the URL of the image you want to analyze.
-    body = "{ 'url': 'http://www.freepngimg.com/download/happy_person/2-2-happy-person-free-download-png.png' }"
+
+    body = "{ 'url': 'http://everydayshouldbefun.com/wp-content/uploads/2017/01/1435305770-36a7c3951a2bb484f033814ee652156a-600x398.jpg' }"
     emotionFound = ''
     Emotions = ['fear','contempt','anger','sadness','surprise','neutral','disgust','happiness']
     try:
@@ -65,6 +75,7 @@ def EmotionTemp(request):
         data = response.read()
         my_json = data.decode('utf8')
         data = json.loads(my_json)
+        print(data)
         maxval = 0
         for emotion in Emotions:
             if maxval < data[0]['scores'][emotion]:
@@ -75,4 +86,4 @@ def EmotionTemp(request):
     except Exception as e:
         print(e.args)
 
-    return render(request,'emotion.html',{'emotionFoundtag':emotionfound})
+    return render(request,'emotion.html',{'emotionFoundtag':emotionfound,'songlist':songList})
