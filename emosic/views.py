364b3b2a5a2a5288aa895446e8c5a6599ad8d849
ajay kundu <ajaykunduktl2@gmail.com>
2017-11-22 23:44:27 +0530

    Some of last error fixed

--- emosic/views.py
+++ emosic/views.py
@@ -55,6 +55,8 @@ def save_image(request):
         return HttpResponse('no data')
 
 
+
+
 import http.client, urllib.request, urllib.parse, urllib.error, base64, sys
 import json
 from musicapp import views
@@ -62,6 +64,64 @@ from musicapp.models import Song
 import shutil
 import random
 import string
+
+
+
+import cv2
+print(cv2.__version__)
+import glob
+import random
+import numpy as np
+
+SIZE_FACE=48
+EMOTIONS = ['angry', 'happy', 'sad','neutral']#emotion list
+fishface = cv2.face.LBPHFaceRecognizer_create()#Initialize fisher face classifier
+
+data = {}
+
+def get_data(): #Define function to get file list, randomly shuffle it and split 80/20
+    images=np.load('data_train.npy')
+    labels= np.load('labels_train.npy')
+    images_test = np.load('data_test.npy')
+    images_test = images_test[:300]
+    labels_test = np.load('labels_test.npy')
+    labels_test = labels_test[:300]
+    images=images.reshape([-1, SIZE_FACE, SIZE_FACE, 1])
+    images_test = images_test.reshape([-1, SIZE_FACE, SIZE_FACE, 1])
+    return images,labels,images_test,labels_test
+
+
+def run_recognizer():
+    training_data, training_labels, prediction_data, prediction_labels = get_data()
+
+    print("training fisher face classifier")
+    print("size of training set is:", len(training_labels), "images")
+    fishface.train(training_data, np.asarray(training_labels))
+    print("predicting classification set: ",len(prediction_labels))
+    cnt = 0
+    correct = 0
+    incorrect = 0
+    for image in prediction_data:
+        pred, conf = fishface.predict(image)
+        print(EMOTIONS[pred])
+        if pred == prediction_labels[cnt]:
+            correct += 1
+            cnt += 1
+        else:
+            incorrect += 1
+            cnt += 1
+    return ((100*correct)/(correct + incorrect))
+
+#Now run it
+metascore = []
+correct=0
+for i in range(0,1):
+    # correct = run_recognizer()
+    print("got", correct, "percent correct!")
+    metascore.append(correct)
+
+print("\n\nend score:", np.mean(metascore), "percent correct!")
+
 
 
 def EmotionTemp(request):
